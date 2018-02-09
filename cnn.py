ba83654aa2b1ac399cd2f95860ac25a37308a9f7
Jack Trute <jack@trute.co.uk>
2018-02-09 14:56:27 +0000

    Started CNN. Simplified extraction functions. Changes to music auth.

---
+++ cnn.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+"""A demonstration of a Convolutional Neural Network."""
+
+# Import packages.
+import os
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+# My imports.
+import extraction_model as model
+from constants import EMOTIONS_5
+
+# Required methods.
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+
+# Imports for Keras.
+import keras
+from keras.models import Sequential, Input, Model
+from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+
+
+def demo_images(X_train, y_train, X_test, y_test):
+    """Demo the first images and truths in dataset."""
+    plt.figure(figsize=[5, 5])
+
+    # Display the first image in training data.
+    plt.subplot(121)
+    plt.imshow(X_train[0, :, :], cmap='gray')
+    plt.title("Ground Truth : {}".format(y_train[0]))
+
+    # Display the first image in testing data
+    plt.subplot(122)
+    plt.imshow(X_test[0, :, :], cmap='gray')
+    plt.title("Ground Truth : {}".format(y_test[0]))
+
+    plt.show()
+
+
+def plot_chart():
+    """."""
+    accuracy = cnn_train.history['acc']
+    val_accuracy = cnn_train.history['val_acc']
+    loss = cnn_train.history['loss']
+    val_loss = cnn_train.history['val_loss']
+    epochs = range(len(accuracy))
+    plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+    plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+
+print(__doc__)
+
+# Start the script.
+script_name = os.path.basename(__file__)  # The name of this script
+print("\n{}: Beginning CNN test...\n".format(script_name))
+start = time.clock()  # Start of the speed test. clock() is most accurate
+
+tdata, tlabels, pdata, plabels = model.get_sets_as_images(EMOTIONS_5)
+
+# Convert to readable arrays.
+X_train = model.convert_numpy(tdata)
+y_train = model.convert_numpy(tlabels)
+X_test = model.convert_numpy(pdata)
+y_test = model.convert_numpy(plabels)
+# print('Training data shape : ', X_train.shape, y_train.shape)
+# print('Testing data shape : ', X_test.shape, y_test.shape)
+
+# Find the unique numbers from the train labels.
+classes = np.unique(y_train)
+nClasses = len(classes)
+# print('Total number of outputs : ', nClasses)
+# print('Output classes : ', classes)
+
+# Convert each 380x380 image into a 380x380x1 matrix.
+X_train = X_train.reshape(-1, 380, 380, 1)
+X_test = X_test.reshape(-1, 380, 380, 1)
+# print('Training data shape : ', X_train.shape)
+# print('Testing data shape : ', X_test.shape)
+
+# Convert from int8 to float32 format, and scale the pixels.
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.
+X_test = X_test / 255.
+
+# Change the labels from categorical to one-hot encoding.
+y_train_one_hot = to_categorical(y_train)
+y_test_one_hot = to_categorical(y_test)
+# print('Original label : ', y_train[0])
+# print('After conversion to one-hot : ', train_Y_one_hot[0])
+
+# 80/20 split.
+X_train, X_valid, train_lbl, valid_lbl = train_test_split(X_train,
+                                                          y_train_one_hot,
+                                                          test_size=0.2,
+                                                          random_state=13)
+# print(X_train.shape, X_valid.shape, train_lbl.shape, valid_lbl.shape)
+
+batch_size = 64
+epochs = 20
+num_classes = 5
+
+cnn_model = Sequential()
+cnn_model.add(Conv2D(32, kernel_size=(3, 3), activation='linear',
+                     input_shape=(380, 380, 1), padding='same'))
+cnn_model.add(Activation('tanh'))
+cnn_model.add(MaxPooling2D((2, 2), padding='same'))
+cnn_model.add(Conv2D(64, (3, 3), activation='linear', padding='same'))
+cnn_model.add(Activation('tanh'))
+cnn_model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+cnn_model.add(Conv2D(128, (3, 3), activation='linear', padding='same'))
+cnn_model.add(Activation('tanh'))
+cnn_model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+cnn_model.add(Flatten())
+cnn_model.add(Dense(128, activation='linear'))
+cnn_model.add(Activation('tanh'))
+cnn_model.add(Dense(num_classes, activation='softmax'))
+
+cnn_model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+# cnn_model.summary()
+
+cnn_train = cnn_model.fit(X_train, train_lbl, batch_size=batch_size,
+                          epochs=epochs, verbose=1,
+                          validation_data=(X_valid, valid_lbl))
+
+test_eval = cnn_model.evaluate(X_test, y_test_one_hot, verbose=0)
+print('Test loss : ', test_eval[0])
+print('Test accuracy : ', test_eval[1])
---
+++ extraction_model.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python3
+
+"""Extraction of features, vectors, and faces, are held in the helper model."""
+
+# Import packages.
+import numpy as np
+import cv2
+import dlib
+import math
+import random
+import glob
+
+from constants import HAAR, HAAR2, HAAR3, HAAR4, PRED
+
+# Set Face Detectors.
+faceDet = cv2.CascadeClassifier(HAAR)
+faceDet2 = cv2.CascadeClassifier(HAAR2)
+faceDet3 = cv2.CascadeClassifier(HAAR3)
+faceDet4 = cv2.CascadeClassifier(HAAR4)
+faceDet5 = dlib.get_frontal_face_detector()  # dlib's face detector
+
+# Build the required objects.
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+predictor = dlib.shape_predictor(PRED)
+
+# Default sizes of training and testing sets (as ratios of the dataset)
+training_set_size = 0.8
+testing_set_size = 0.2
+
+
+def convert_numpy(data):
+    """Convert given data into numpy array."""
+    return np.array(data)
+
+
+def get_images(emotion):
+    """Split dataset into 80 percent training set and 20 percent prediction."""
+    files = glob.glob("sort_database//database//{}//*".format(emotion))
+    random.shuffle(files)
+    # training = files[:int(len(files) * training_set_size)]
+    # prediction = files[-int(len(files) * testing_set_size):]
+    # for testing
+    training = files[:5]
+    prediction = files[-5:]
+    return training, prediction
+
+
+def get_face_opencv_vectors(img):
+    """Get all the faces as OpenCV vectors."""
+    haar_detections = []
+    facefeatures = []
+
+    # Use dlib to detect faces in the frame.
+    detections = faceDet5(img, 1)
+
+    if len(detections) > 0:  # dlib found faces
+        for face in detections:
+            x = face.left()
+            y = face.top()
+            w = face.right() - face.left()
+            h = face.bottom() - face.top()
+            facefeatures.append([x, y, w, h])
+    else:
+        haar_detections = faceDet.detectMultiScale(img, scaleFactor=1.1,
+                                                   minNeighbors=10,
+                                                   minSize=(5, 5),
+                                                   flags=cv2.CASCADE_SCALE_IMAGE)
+        if len(haar_detections) > 0:  # HAAR Cascade found faces
+            facefeatures = haar_detections
+
+        if len(facefeatures) == 0:
+            haar_detections2 = faceDet2.detectMultiScale(img, scaleFactor=1.1,
+                                                         minNeighbors=10,
+                                                         minSize=(5, 5),
+                                                         flags=cv2.CASCADE_SCALE_IMAGE)
+            if len(haar_detections2) > 0:  # HAAR Cascade 2 found faces
+                facefeatures = haar_detections2
+
+        if len(facefeatures) == 0:
+            haar_detections3 = faceDet3.detectMultiScale(img, scaleFactor=1.1,
+                                                         minNeighbors=10,
+                                                         minSize=(5, 5),
+                                                         flags=cv2.CASCADE_SCALE_IMAGE)
+            if len(haar_detections3) > 0:  # HAAR Cascade 3 found faces
+                facefeatures = haar_detections3
+
+        if len(facefeatures) == 0:
+            haar_detections4 = faceDet4.detectMultiScale(img, scaleFactor=1.1,
+                                                         minNeighbors=10,
+                                                         minSize=(5, 5),
+                                                         flags=cv2.CASCADE_SCALE_IMAGE)
+            if len(haar_detections4) > 0:  # HAAR Cascade 4 found faces
+                facefeatures = haar_detections4
+            else:
+                print("No face found")
+
+    return facefeatures
+
+
+def get_face_dlib_rects(img):
+    """Get all the faces as dlib Rectangles."""
+    haar_detections = []
+
+    # Use dlib to detect faces in the frame.
+    detections = faceDet5(img, 1)
+
+    if len(detections) > 0:
+        print("DLIB")
+        return detections
+    else:
+        haar_detections = faceDet.detectMultiScale(img, scaleFactor=1.1,
+                                                   minNeighbors=10,
+                                                   minSize=(5, 5),
+                                                   flags=cv2.CASCADE_SCALE_IMAGE)
+        if len(haar_detections) > 0:  # HAAR Cascade found faces
+            for (x, y, w, h) in haar_detections:
+                print("HAAR")
+                dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+                detections.append(dlib_rect)
+                break
+
+        if len(detections) == 0:
+            haar_detections2 = faceDet2.detectMultiScale(img, scaleFactor=1.1,
+                                                         minNeighbors=10,
+                                                         minSize=(5, 5),
+                                                         flags=cv2.CASCADE_SCALE_IMAGE)
+            if len(haar_detections2) > 0:  # HAAR Cascade 2 found faces
+                print("HAAR2")
+                for (x, y, w, h) in haar_detections2:
+                    dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+                    detections.append(dlib_rect)
+                    break
+
+        if len(detections) == 0:
+            haar_detections3 = faceDet3.detectMultiScale(img, scaleFactor=1.1,
+                                                         minNeighbors=10,
+                                                         minSize=(5, 5),
+                                                         flags=cv2.CASCADE_SCALE_IMAGE)
+            if len(haar_detections3) > 0:  # HAAR Cascade 3 found faces
+                for (x, y, w, h) in haar_detections3:
+                    print("HAAR3")
+                    dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+                    detections.append(dlib_rect)
+                    break
+
+        if len(detections) == 0:
+            haar_detections4 = faceDet4.detectMultiScale(img, scaleFactor=1.1,
+                                                         minNeighbors=10,
+                                                         minSize=(5, 5),
+                                                         flags=cv2.CASCADE_SCALE_IMAGE)
+            if len(haar_detections4) > 0:  # HAAR Cascade 4 found faces
+                for (x, y, w, h) in haar_detections4:
+                    print("HAAR4")
+                    dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+                    detections.append(dlib_rect)
+                    break
+            else:
+                print("No face found")
+
+    return detections
+
+
+def get_face_landmarks(img):
+    """Get all the faces as flat landmark vectors."""
+    detections = get_face_dlib_rects(img)
+
+    # We may detect 0, 1 or many faces. Loop through each face detected.
+    for i, j in enumerate(detections):
+        # Draw facial landmarks with the predictor class.
+        shape = predictor(img, j)
+        xlist = []
+        ylist = []
+
+        # Store X and Y coordinates in separate lists.
+        for i in range(1, 68):  # 68 because we're looking for 68 landmarks
+            xlist.append(float(shape.part(i).x))
+            ylist.append(float(shape.part(i).y))
+
+        # Find both coordinates for the centre of gravity (middle point).
+        xmean = np.mean(xlist)
+        ymean = np.mean(ylist)
+
+        # Calculate the distance from centre to other points in both axes.
+        xcentral = [(x-xmean) for x in xlist]
+        ycentral = [(y-ymean) for y in ylist]
+
+        # Condition the vectors.
+        landmarks_vectorised = []
+        for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
+            landmarks_vectorised.append(w)
+            landmarks_vectorised.append(z)
+            meannp = np.asarray((ymean, xmean))
+            coornp = np.asarray((z, w))
+            dist = np.linalg.norm(coornp-meannp)
+            landmarks_vectorised.append(dist)
+            landmarks_vectorised.append((math.atan2(y, x)*360)/(math.pi*2))
+
+    if len(detections) == 0:
+        return "error"
+    return landmarks_vectorised
+
+
+def get_sets_as_images(emotions):
+    """Make the sets to test on."""
+    training_data = []
+    training_labels = []
+    prediction_data = []
+    prediction_labels = []
+
+    for emotion in emotions:
+        print("Obtaining images that represent ``{}''.".format(emotion))
+        training, prediction = get_images(emotion)
+
+        print("Allocating {}. This directory contains {} images."
+              .format(emotion, len(training)+len(prediction)))
+        # Append data to training and prediction lists and label 0 to num.
+        for item in training:
+            image = cv2.imread(item)  # Open image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            clahe_image = clahe.apply(gray)
+            check = get_face_dlib_rects(clahe_image)
+
+            if len(check) > 0:
+                training_data.append(clahe_image)
+                training_labels.append(emotions.index(emotion))
+
+        for item in prediction:
+            image = cv2.imread(item)  # Open image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            clahe_image = clahe.apply(gray)
+            check = get_face_dlib_rects(clahe_image)
+
+            if len(check) > 0:
+                prediction_data.append(clahe_image)
+                prediction_labels.append(emotions.index(emotion))
+
+    return training_data, training_labels, prediction_data, prediction_labels
